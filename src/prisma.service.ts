7cf3d29c30dede35c1904dcbede6d24be8894909
LifeBits <mail@lifebits.ru>
2022-01-06 09:03:47 +0700

    Init Prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnModuleDestroy, OnModuleInit, INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.disconnect()
+  }
+
+  // async enableShutdownHooks(app: INestApplication) {
+  //   this.$on('', async () => {
+  //     await app.close();
+  //   });
+  // }
+}
